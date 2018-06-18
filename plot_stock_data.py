deb45688a111ec0948bef31225c37c2e5c1ee5f3
Tana <run@run.com>
2018-06-18 11:12:30 -0400

    Test code to check whether bokeh works

--- plot_stock_data.py
+++ plot_stock_data.py
@@ -40,5 +40,24 @@ def plot_stock_data(ticker_symbol, options):
     script, div = components(p)
     
     print 'Script = ', script
+    print 'Div= ', div
+    
+
+    # prepare some data
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+
+    # output to static HTML file
+    output_file("lines.html")
+
+    # create a new plot with a title and axis labels
+    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+    # add a line renderer with legend and line thickness
+    p.line(x, y, legend="Temp.", line_width=2)
+
+    # show the results
+    show(p)
+
     return render_template("plot.html", script=script, div=div, company="")
     

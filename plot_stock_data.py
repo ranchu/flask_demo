3de1ea5b6558948d2ef1db7b20d45d3935329181
Tana <run@run.com>
2018-06-18 14:46:01 -0400

    Added JS validation.

--- plot_stock_data.py
+++ plot_stock_data.py
@@ -7,31 +7,29 @@ import pandas as pd
 def plot_stock_data(ticker_symbol, options):
     do_cls, do_open, do_high, do_low = options
     
-    print 'Plot stock data method'
 
     url= "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker_symbol+"&apikey=U4AV38IHD6RSSF9Q&datatype=csv" 
     
+
     ## Using pandas to process the data.
     stock_data = pd.read_csv(url)
     stock_data  = stock_data.sort_values(by='timestamp')  
     stock_data['timestamp'] = pd.to_datetime(stock_data.timestamp, errors='coerce')
     stock_data = stock_data.set_index('timestamp')  
      
-
+    ## The API returns data of last 100 days.
+    stock_data = stock_data[:30]
     ## Plotting using Bokeh
-    p = figure(title="Stock data", x_axis_label='Date', y_axis_label='Stock price')
+    p = figure(#title="Stock data", 
+            x_axis_label='Date', y_axis_label='Stock price', x_axis_type='datetime')
     
     if do_open:
-        print 'Plotting opening price'
         p.line(stock_data.index.values, stock_data.open.values, legend="Open", line_width=2, color='blue')
     if do_cls:
-        print 'Plotting closing price'
         p.line(stock_data.index.values, stock_data.close.values, legend="Close", line_width=2, color='red')
     if do_high:
-        print 'Plotting high price'
         p.line(stock_data.index.values, stock_data.high.values, legend="High", line_width=2, color='cyan')
     if do_low:
-        print 'Plotting low price'
         p.line(stock_data.index.values, stock_data.low.values, legend="Low", line_width=2, color='black')
 
     p.xaxis[0].formatter = DatetimeTickFormatter()
@@ -39,25 +37,5 @@ def plot_stock_data(ticker_symbol, options):
 
     script, div = components(p)
     
-    print 'Script = ', script
-    print 'Div= ', div
-    
-
-    # prepare some data
-    x = [1, 2, 3, 4, 5]
-    y = [6, 7, 2, 4, 5]
-
-    # output to static HTML file
-    output_file("lines.html")
-
-    # create a new plot with a title and axis labels
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-
-    # add a line renderer with legend and line thickness
-    p.line(x, y, legend="Temp.", line_width=2)
-
-    # show the results
-    show(p)
-
     return render_template("plot.html", script=script, div=div, company="")
     

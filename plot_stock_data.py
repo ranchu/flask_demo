c67a5ac67c0b32e7862f3ec6a6ab05c3fc0dcb9f
Tana <run@run.com>
2018-06-18 10:57:42 -0400

    Adding logs

--- plot_stock_data.py
+++ plot_stock_data.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 def plot_stock_data(ticker_symbol, options):
     do_cls, do_open, do_high, do_low = options
+    
+    print 'Plot stock data method'
 
     url= "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker_symbol+"&apikey=U4AV38IHD6RSSF9Q&datatype=csv" 
     
@@ -20,12 +22,16 @@ def plot_stock_data(ticker_symbol, options):
     p = figure(title="Stock data", x_axis_label='Date', y_axis_label='Stock price')
     
     if do_open:
+        print 'Plotting opening price'
         p.line(stock_data.index.values, stock_data.open.values, legend="Open", line_width=2, color='blue')
     if do_cls:
+        print 'Plotting closing price'
         p.line(stock_data.index.values, stock_data.close.values, legend="Close", line_width=2, color='red')
     if do_high:
+        print 'Plotting high price'
         p.line(stock_data.index.values, stock_data.high.values, legend="High", line_width=2, color='cyan')
     if do_low:
+        print 'Plotting low price'
         p.line(stock_data.index.values, stock_data.low.values, legend="Low", line_width=2, color='black')
 
     p.xaxis[0].formatter = DatetimeTickFormatter()
@@ -33,5 +39,6 @@ def plot_stock_data(ticker_symbol, options):
 
     script, div = components(p)
     
+    print 'Script = ', script
     return render_template("plot.html", script=script, div=div, company="")
     

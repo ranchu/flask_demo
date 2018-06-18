7a8a4affc1238345827e2da69922ac34f08494cf
Tana <run@run.com>
2018-06-18 09:56:14 -0400

    First version

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from flask import Flask, render_template, request, redirect
+from plot_stock_data import plot_stock_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    app.logger.error('Calling index method')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+
+@app.route('/plot', methods=['POST'])
+def plot():
+    app.logger.info('Calling plot method')
+    form = request.form
+    ticker = form['ticker']
+    features = form.getlist('features')
+
+    if features:
+        do_open = 'open' in features
+        do_close= 'close' in features
+        do_high = 'high' in features
+        do_low= 'low' in features
+        options= (do_open, do_close, do_high, do_low)
+        return plot_stock_data(ticker, options)
+    else:
+        ##TODO: Error page, JS form validation, ticker name mapping.
+
+        return "Error page"
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=5000, debug=True)
---
+++ plot_stock_data.py
@@ -0,0 +1,37 @@
+from bokeh.plotting import figure, output_file, show
+from flask import Flask, render_template, request
+from bokeh.embed import components
+from bokeh.models import DatetimeTickFormatter
+import pandas as pd
+
+def plot_stock_data(ticker_symbol, options):
+    do_cls, do_open, do_high, do_low = options
+
+    url= "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ticker_symbol+"&apikey=U4AV38IHD6RSSF9Q&datatype=csv" 
+    
+    ## Using pandas to process the data.
+    stock_data = pd.read_csv(url)
+    stock_data  = stock_data.sort_values(by='timestamp')  
+    stock_data['timestamp'] = pd.to_datetime(stock_data.timestamp, errors='coerce')
+    stock_data = stock_data.set_index('timestamp')  
+     
+
+    ## Plotting using Bokeh
+    p = figure(title="Stock data", x_axis_label='Date', y_axis_label='Stock price')
+    
+    if do_open:
+        p.line(stock_data.index.values, stock_data.open.values, legend="Open", line_width=2, color='blue')
+    if do_cls:
+        p.line(stock_data.index.values, stock_data.close.values, legend="Close", line_width=2, color='red')
+    if do_high:
+        p.line(stock_data.index.values, stock_data.high.values, legend="High", line_width=2, color='cyan')
+    if do_low:
+        p.line(stock_data.index.values, stock_data.low.values, legend="Low", line_width=2, color='black')
+
+    p.xaxis[0].formatter = DatetimeTickFormatter()
+    p.legend.location = "top_left"
+
+    script, div = components(p)
+    
+    return render_template("plot.html", script=script, div=div, company="")
+    
